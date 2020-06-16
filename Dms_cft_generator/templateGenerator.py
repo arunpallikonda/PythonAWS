3b96e4a3c88d0503921d4def0277b9189e9314be
Arun <arun.pallikonda1@gmail.com>
2020-06-16 06:47:23 -0500

    Inital commit

--- Dms_cft_generator/templateGenerator.py
+++ Dms_cft_generator/templateGenerator.py
@@ -8,6 +8,8 @@ from collections import OrderedDict
 from jsonschema import validate
 import boto3
 import argparse
+import base64
+from botocore.exceptions import ClientError
 
 BASE_DIR = os.path.abspath(os.getcwd())
 
@@ -90,7 +92,7 @@ def deployCloudformation(templateJson):
             break
 
 
-def getpassword(userName):
+def getPasswordFromPam(userName):
     # TODO: Do PAM integration here...
     return "dummypassword"
 
@@ -106,10 +108,53 @@ def updateSourceEndpointDetailsInNewEndpointsTemplate(inputJson, templateJson):
     templateJson['Resources']['SourceEndpoint']['Properties']['Port'] = inputJson['SourceEndpointDetails']['Port']
     templateJson['Resources']['SourceEndpoint']['Properties']['Username'] = inputJson['SourceEndpointDetails'][
         'Username']
-    templateJson['Resources']['SourceEndpoint']['Properties']['Password'] = getpassword(
+    templateJson['Resources']['SourceEndpoint']['Properties']['Password'] = getPasswordFromPam(
         inputJson['SourceEndpointDetails']['Username'])
+    templateJson['Resources']['SourceEndpoint']['Properties']['DatabaseName'] = inputJson['SourceEndpointDetails'][
+        'DatabaseName']
 
     # templateJson['Resources']['SourceEndpoint']['Properties']['Port']
+
+
+def getPasswordFromSecretsManager(param):
+    secret_name = "OracleSecret"
+
+    # Create a Secrets Manager client
+    client = boto3.client(service_name='secretsmanager', region_name=r"us-east-1")
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+    secretJson = json.loads(secret)
+    return secretJson['password']
 
 
 def updateTargetEndpointDetailsInNewEndpointsTemplate(inputJson, templateJson):
@@ -123,8 +168,10 @@ def updateTargetEndpointDetailsInNewEndpointsTemplate(inputJson, templateJson):
     templateJson['Resources']['TargetEndpoint']['Properties']['Port'] = inputJson['TargetEndpointDetails']['Port']
     templateJson['Resources']['TargetEndpoint']['Properties']['Username'] = inputJson['TargetEndpointDetails'][
         'Username']
-    templateJson['Resources']['TargetEndpoint']['Properties']['Password'] = getpassword(
+    templateJson['Resources']['TargetEndpoint']['Properties']['Password'] = getPasswordFromSecretsManager(
         inputJson['TargetEndpointDetails']['Username'])
+    templateJson['Resources']['TargetEndpoint']['Properties']['DatabaseName'] = inputJson['TargetEndpointDetails'][
+        'DatabaseName']
 
     # templateJson['Resources']['TargetEndpoint']['Properties']['Port']
 
@@ -183,7 +230,7 @@ if __name__ == '__main__':
                     outputTemplateFileName = str(eachInputFile.split('.')[0]) + "-cft.json"
                     with open(os.path.join(BASE_DIR, "accounts", eachAccount, "outputs", outputTemplateFileName),
                               'w') as outputFile:
-                        json.dump(templateJson, outputFile)
+                        json.dump(templateJson, outputFile, indent=4)
                     deployCloudformation(templateJson)
                 else:
                     print("Invalid templateType. Please check inputJson: " + os.path.join(BASE_DIR, "accounts",
