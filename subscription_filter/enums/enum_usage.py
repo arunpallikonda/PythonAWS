19fe88ce985802a7e443a410cf700281d429dcb8
vidyaprabhakarsristi <sristi.vidyap@gmail.com>
2020-07-13 16:49:39 -0500

    Local Commit

---
+++ subscription_filter/enums/enum_usage.py
@@ -0,0 +1,3 @@
+from subscription_filter.enums.sample_enum import SampleEnum
+
+print(SampleEnum.CALL_CODE.value)
---
+++ subscription_filter/enums/sample_enum.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class SampleEnum(enum.Enum):
+    CALL_CODE = 'callStrike'
+    PUT_CODE = 'putStrike'
